cdd64fc203963910ea2a3936e2e7eedd46a14e75
OzcanKaygi <ozcankaygi@hotmail.com>
2022-12-25 22:53:05 +0100

    v1

--- data.js
+++
@@ -1,7 +0,0 @@
-const XLSX = require("xlsx");
-
-const workbook = XLSX.readFile("data.xlsx");
-const worksheet = workbook.Sheets["Sheet1"];
-
-const arrStudents = XLSX.utils.sheet_to_json(worksheet);
-console.log(arrStudents)
